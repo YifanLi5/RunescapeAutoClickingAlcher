f689c66f433a6899ad11deb8c1b9641d20adf6a8
yifanli5 <yifanli5@asu.edu>
2017-12-08 05:58:24 -0700

    asdf

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pyautogui
 import numpy
+import os
 pyautogui.PAUSE = 1
 GAME_TICK_INTERVAL = 0.603
 print(pyautogui.size())
@@ -8,7 +9,6 @@ print(pyautogui.size())
 
 def main():
     numAlchs, topLeftBounds, bottomRightBounds = takeInput()
-    print numAlchs, topLeftBounds, bottomRightBounds
     try:
         while numAlchs > 0:
             if not isCursorOnAlch(topLeftBounds, bottomRightBounds):
@@ -20,27 +20,31 @@ def main():
             
 
     except KeyboardInterrupt:
-        print("/nDone")
+        print("Done")
 
 def isCursorOnAlch(topLeftBounds, bottomRightBounds):
     currPos = pyautogui.position()
     temp = (currPos[0] > topLeftBounds[0] and currPos[0] < bottomRightBounds[0] and currPos[1] > topLeftBounds[1] and currPos[1] < bottomRightBounds[1])
-    print temp
     return temp
 
 def takeInput():
-    numAlchs = input("1.) enter number of alchs: ")
-    raw_input("2.) place cursor on approximate TOP LEFT of alch icon\nmake sure that you can still cast high alch then press ENTER")
+    numAlchs = int(input("1.) enter number of alchs: "))
+    input("2.) place cursor on approximate TOP LEFT of alch icon\nmake sure that you can still cast high alch then press ENTER")
     topLeftCoordinates = pyautogui.position()
     print("top left: ", str(topLeftCoordinates))
-    raw_input("3.) place cursor on approximate BOTTOM RIGHT of alch icon\nmake sure that you can still cast high alch then press ENTER")
+    input("3.) place cursor on approximate BOTTOM RIGHT of alch icon\nmake sure that you can still cast high alch then press ENTER")
     bottomRightCoordinates = pyautogui.position()
     print("bottom right: ", str(bottomRightCoordinates))
+    input("4.) place cursor on alch icon then press ENTER")
+    cls()
     return (numAlchs, topLeftCoordinates, bottomRightCoordinates)
 
 
 def setNextPauseInterval():
     pyautogui.PAUSE = numpy.random.normal(GAME_TICK_INTERVAL*5, 0.5)
+
+def cls():
+    os.system('cls' if os.name=='nt' else 'clear')
     
 
 if __name__ == "__main__":

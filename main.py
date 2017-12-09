ed29e5b93fa8047cc4af929bab70574bd129083f
Yifan Li <yifanli5@asu.edu>
2017-12-08 20:54:49 -0700

    added better prints, added ability to move mouse back into position

--- main.py
+++ main.py
@@ -1,53 +1,82 @@
 import pyautogui
 import numpy
+import random
+import time
 import os
+
 pyautogui.PAUSE = 1
 GAME_TICK_INTERVAL = 0.603
-print(pyautogui.size())
-
-
+DOUBLE_CLICK_INTERVAL = 0.35
+DOUBLE_CLICK_INTERVAL_STD_DEV = 0.05
 
 def main():
+
     numAlchs, topLeftBounds, bottomRightBounds = takeInput()
+    numAlchsRemaining = numAlchs
     try:
-        while numAlchs > 0:
-            if not isCursorOnAlch(topLeftBounds, bottomRightBounds):
-                print("out of bounds")
-                pass #TODO: set cursor into position   
-            pyautogui.click()
-            pyautogui.PAUSE = numpy.random.normal(0.35, 0.05)
-            pyautogui.click()
-            numAlchs -= 1
-            setNextPauseInterval()
-            
+        while numAlchsRemaining > 0:
+            if not isCursorOnAlchBounds(topLeftBounds, bottomRightBounds):
+                boundsWarningMsgBase = "\rOut of alching bounds!, moving mouse in "
+                countdown = 10
+                while countdown > 0:
+                    boundsWarningMsg = boundsWarningMsgBase + str(countdown) + "s"
+                    print(boundsWarningMsg, end='', flush=True)
+                    countdown -= 1
+                    time.sleep(1)
+                moveCursorBackInBounds(topLeftBounds, bottomRightBounds)
+                cls()
+            else:
+                numAlchsRemaining = castAlch(numAlchsRemaining, numAlchs)
+
 
     except KeyboardInterrupt:
         print("Done")
 
-def isCursorOnAlch(topLeftBounds, bottomRightBounds):
+def isCursorOnAlchBounds(topLeftBounds, bottomRightBounds):
     currPos = pyautogui.position()
     temp = (currPos[0] > topLeftBounds[0] and currPos[0] < bottomRightBounds[0] and currPos[1] > topLeftBounds[1] and currPos[1] < bottomRightBounds[1])
     return temp
+
+def castAlch(numAlchsRemaining, numAlchs):
+    pyautogui.click()
+    pyautogui.PAUSE = numpy.random.normal(0.35, 0.05)
+    pyautogui.click()
+    numAlchsRemaining -= 1
+    numAlchsRemainingStr = "\r"+str(numAlchsRemaining) + " of " + str(numAlchs) + " alchs remaining."
+    print(numAlchsRemainingStr, end='', flush=True)
+    setNextPauseIntervalBetweenAlchs()
+    return numAlchsRemaining
+
 
 def takeInput():
     numAlchs = int(input("1.) enter number of alchs: "))
     input("2.) place cursor on approximate TOP LEFT of alch icon\nmake sure that you can still cast high alch then press ENTER")
     topLeftCoordinates = pyautogui.position()
-    print("top left: ", str(topLeftCoordinates))
+    print("selected top left: ", str(topLeftCoordinates))
     input("3.) place cursor on approximate BOTTOM RIGHT of alch icon\nmake sure that you can still cast high alch then press ENTER")
     bottomRightCoordinates = pyautogui.position()
-    print("bottom right: ", str(bottomRightCoordinates))
+    print("selected bottom right: ", str(bottomRightCoordinates))
     input("4.) place cursor on alch icon then press ENTER")
     cls()
     return (numAlchs, topLeftCoordinates, bottomRightCoordinates)
 
 
-def setNextPauseInterval():
-    pyautogui.PAUSE = numpy.random.normal(GAME_TICK_INTERVAL*4.5, 0.25)
+def setNextPauseIntervalBetweenAlchs():
+    pyautogui.PAUSE = numpy.random.normal(GAME_TICK_INTERVAL*4.5, 0.45)
+
+def moveCursorBackInBounds(topLeftBounds, bottomRightBounds):
+    #find X and Y range within bounds
+    minX = topLeftBounds[0]
+    maxX = bottomRightBounds[0]
+    minY = topLeftBounds[1]
+    maxY = bottomRightBounds[1]
+    #generate coordinates between maxX, minX, maxY, minY and move there
+    randX = random.randint(minX,maxX)
+    randY = random.randint(minY,maxY)
+    pyautogui.moveTo(randX, randY, 0.3)
 
 def cls():
     os.system('cls' if os.name=='nt' else 'clear')
-    
 
 if __name__ == "__main__":
     main()
